5ad11d5ec7f9ba7380d1bd57f101555dbb083a7c
Yahaya Abdul Raqeeb <101267653+Yahaya-Abdul-Raqeeb@users.noreply.github.com>
2022-03-30 14:53:52 +0000

    added routes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import withLayout from './withLayout'
 
-function Home() {
+function About() {
   return (
  <div>
-   <h1>Welcome To My HomePage</h1> 
+   <h1>About Me</h1> 
     <div>
     <p>On 28 August, Chelsea travelled to Anfield to take on Liverpool in a match that was billed as an early test for two title contenders.[10] Kai Havertz opened the scoring off a corner kick with a looping near-post header that spiraled over goalkeeper Alisson.[11] In the first half stoppage time, Reece James was sent off after referee Anthony Taylor consulted the VAR and adjudged that James had used his arm to prevent a goal. Mohammed Salah scored the ensuing penalty despite furious protests from Chelsea players. In the second half, Chelsea defended deeply and compactly and the match finished 1–1.[12]
 
@@ -14,7 +14,8 @@ function Home() {
 
     </p>
 
-    <p>On 28 August, Chelsea travelled to Anfield to take on Liverpool in a match that was billed as an early test for two title contenders.[10] Kai Havertz opened the scoring off a corner kick with a looping near-post header that spiraled over goalkeeper Alisson.[11] In the first half stoppage time, Reece James was sent off after referee Anthony Taylor consulted the VAR and adjudged that James had used his arm to prevent a goal. Mohammed Salah scored the ensuing penalty despite furious protests from Chelsea players. In the second half, Chelsea defended deeply and compactly and the match finished 1–1.[12]
+    <p>On 28 August, Chelsea travelled to Anfield to take on Liverpool in a match that was billed as an early test for two title contenders.
+    [10] Kai Havertz opened the scoring off a corner kick with a looping near-post header that spiraled over goalkeeper Alisson.[11] In the first half stoppage time, Reece James was sent off after referee Anthony Taylor consulted the VAR and adjudged that James had used his arm to prevent a goal. Mohammed Salah scored the ensuing penalty despite furious protests from Chelsea players. In the second half, Chelsea defended deeply and compactly and the match finished 1–1.[12]
 
     </p>
     
@@ -31,4 +32,4 @@ function Home() {
   )
 }
 
-export default withLayout(Home)
+export default withLayout(About)
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import withLayout from './withLayout'
 
-function Home() {
+function Blog() {
   return (
  <div>
-   <h1>Welcome To My HomePage</h1> 
+   <h1>My BlogPage</h1> 
     <div>
     <p>On 28 August, Chelsea travelled to Anfield to take on Liverpool in a match that was billed as an early test for two title contenders.[10] Kai Havertz opened the scoring off a corner kick with a looping near-post header that spiraled over goalkeeper Alisson.[11] In the first half stoppage time, Reece James was sent off after referee Anthony Taylor consulted the VAR and adjudged that James had used his arm to prevent a goal. Mohammed Salah scored the ensuing penalty despite furious protests from Chelsea players. In the second half, Chelsea defended deeply and compactly and the match finished 1–1.[12]
 
@@ -31,4 +31,4 @@ function Home() {
   )
 }
 
-export default withLayout(Home)
+export default withLayout(Blog)
